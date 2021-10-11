8b6665613830bbe50fae85e7bcc619fd4b6c6244
Samuel Lisboa <samuel.lisboa@localiza.com>
2021-10-11 18:33:35 -0300

    versao beta

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import socket
+from random import randint
+from datetime import datetime
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,9 +10,29 @@ api = Api(app)
 class Sena(Resource):
     
     def get(self):
-        return {"jogo": "04 05 06 12 34 54"}
-    
+        return {"Retorno": [
+                            {"Azure instance: " : socket.gethostname(),
+                             "Datetime: ": str(datetime.now()),
+                             "Jogo gerado ": str(self.jogo())
+                            }
+                           ]
+               }
+
+    def jogo(self):
+        numeros=[]
+        sena=[]
+        
+        for n in range(1,61):
+            numeros.append(n)
+            
+        for i in range(6):
+            sena.append(numeros.pop(randint(1, len(numeros)-1)))
+            
+        sena.sort()
+        return sena
+        
+
 api.add_resource(Sena, '/')
 
-if __name__=='__main__':
+if __name__=='__main__': 
     app.run(debug=True)
